52c11ff138c66d63d1c9ffbe85004b6e2b4a8681
Carlos Lozano <carlos@adaptive.me>
2015-06-08 12:37:14 +0200

    Remove direct references.

---
+++ prepare-deps.sh
@@ -0,0 +1,16 @@
+cd che
+git submodule update --recursive --remote
+cd che-core
+mvn clean install
+cd ..
+cd che
+mvn clean install
+cd ..
+cd user-dashboard
+mvn clean install
+cd ..
+cd cli
+mvn clean install
+cd ..
+cd ..
+
