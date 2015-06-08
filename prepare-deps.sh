119a81563c137951b3310176b0ae41c3397c4ff5
Carlos Lozano <carlos@adaptive.me>
2015-06-08 13:15:08 +0200

    Check mvn build status for dependencies.

--- prepare-deps.sh
+++ prepare-deps.sh
@@ -1,16 +1,44 @@
-cd deps
+#!/bin/bash
+
+HOME_DIR=`pwd`
+
+command -v compass >/dev/null 2>&1 || { echo "I require compass but it's not installed. Install with ´sudo gem install compass --pre´  Aborting." >&2; exit 1; }
+cd $HOME_DIR/deps
+
 git submodule update --recursive --remote
-cd che-core
+if [ $? -ne 0 ]; then
+    echo "ERROR: Unable to update submodules."
+    exit 1
+fi
+
+cd $HOME_DIR/deps/che-core
 mvn clean install
-cd ..
-cd che
+if [ $? -ne 0 ]; then
+    echo "ERROR: che-core failed to build."
+    exit 1
+fi
+
+cd $HOME_DIR/deps/che
 mvn clean install
-cd ..
-cd user-dashboard
+if [ $? -ne 0 ]; then
+    echo "ERROR: che failed to build."
+    exit 1
+fi
+
+cd $HOME_DIR/deps/user-dashboard
 mvn clean install
-cd ..
-cd cli
+if [ $? -ne 0 ]; then
+    echo "ERROR: user-dashboard failed to build."
+    exit 1
+fi
+
+cd $HOME_DEPS/deps/cli
 mvn clean install
-cd ..
-cd ..
+if [ $? -ne 0 ]; then
+    echo "ERROR: cli failed to build."
+    exit 1
+fi
+
+cd $HOME_DEPS
+echo "ALL DONE."
 
