a22094633b8dcf8d8bfc662798f4dee48a29f761
Rafael Roman <panthro.rafael@gmail.com>
2015-07-15 16:31:08 +0200

    making it easier to restart in debug mode. Validating maven build.

--- dashbar.sh
+++ dashbar.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 mvn clean install
+if [ $? -ne 0 ]; then
+    echo "[ERROR] MAVEN BUILD FAILURE"
+    exit 1
+fi
 WAR=`find adaptive-assembly-war -iname *.war`
 echo "War built -> "$WAR
 echo "Cleaning up tomcat"
@@ -8,4 +12,10 @@ echo "Copying built war to tomcat"
 cp $WAR deps/che/assembly-sdk/target/tomcat-ide/webapps/che.war
 echo "Forwarding command"
 cd deps/che
-./che.sh $*
+if [ $1 = "restart" ];
+    then
+        ./che.sh stop
+        ./che.sh jpda start
+    else
+        ./che.sh $*
+fi
