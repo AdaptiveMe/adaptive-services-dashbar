67fdb771a1bfe5a59bbb6de7d8a3e20fdbb66b69
Rafael Roman <panthro.rafael@gmail.com>
2015-08-07 17:47:05 +0200

    generator is now also a project importer handler, that can be called upon project import

--- adaptive-che-plugin/src/main/java/me/adaptive/che/plugin/server/project/AdaptiveProjectGenerator.java
+++ adaptive-che-plugin/src/main/java/me/adaptive/che/plugin/server/project/AdaptiveProjectGenerator.java
@@ -24,14 +24,21 @@ import me.adaptive.che.plugin.server.util.SimpleCommandLineExecutor;
 import org.eclipse.che.api.core.ConflictException;
 import org.eclipse.che.api.core.ForbiddenException;
 import org.eclipse.che.api.core.ServerException;
+import org.eclipse.che.api.core.UnauthorizedException;
+import org.eclipse.che.api.core.util.LineConsumer;
+import org.eclipse.che.api.core.util.LineConsumerFactory;
 import org.eclipse.che.api.project.server.FolderEntry;
+import org.eclipse.che.api.project.server.ProjectImporter;
 import org.eclipse.che.api.project.server.handlers.CreateProjectHandler;
 import org.eclipse.che.api.project.server.type.AttributeValue;
 import org.eclipse.che.vfs.impl.fs.VirtualFileImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nullable;
 import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
 import java.util.Map;
 
 /**
@@ -39,7 +46,7 @@ import java.util.Map;
  * Created by panthro on 02/07/15.
  */
 @Singleton
-public class AdaptiveProjectGenerator implements CreateProjectHandler {
+public class AdaptiveProjectGenerator implements CreateProjectHandler, ProjectImporter {
 
 
     private static Logger LOG = LoggerFactory.getLogger(AdaptiveProjectGenerator.class);
@@ -54,23 +61,63 @@ public class AdaptiveProjectGenerator implements CreateProjectHandler {
         //TODO enable only when generate is passed
         //if (options != null && options.containsKey(OPTION_GENERATE)) {
             //TODO check if we should start it in another thread
-            try {
-                File workDir = ((VirtualFileImpl) baseFolder.getVirtualFile()).getIoFile();
-                File generatorLog = ((VirtualFileImpl) baseFolder.createFile(GENERATION_LOG, new byte[0], "text/plain").getVirtualFile()).getIoFile();
-                SimpleCommandLineExecutor executor = new SimpleCommandLineExecutor();
-                executor.execute(new GeneratorCommandBuilder(baseFolder.getVirtualFile().getName()).withAttributes(attributes).withOptions(options), workDir, generatorLog);
-                if (!executor.isSuccess()) {
-                    LOG.warn("There was an error executing the generator command");
-                }
-
-            } catch (Exception e) {
-                LOG.warn("Error executing the generator", e);
-            }
+        generateProject(baseFolder, attributes, options, LineConsumerFactory.NULL);
         //}
     }
+
+    private void generateProject(FolderEntry baseFolder, @Nullable Map<String, AttributeValue> attributes, Map<String, String> options, LineConsumerFactory lineConsumerFactory) {
+        try {
+            File workDir = ((VirtualFileImpl) baseFolder.getVirtualFile()).getIoFile();
+            File generatorLog = ((VirtualFileImpl) baseFolder.createFile(GENERATION_LOG, new byte[0], "text/plain").getVirtualFile()).getIoFile();
+            SimpleCommandLineExecutor executor = new SimpleCommandLineExecutor();
+            LineConsumer consumer = lineConsumerFactory.newLineConsumer();
+            executor.execute(new GeneratorCommandBuilder(baseFolder.getVirtualFile().getName()).withAttributes(attributes).withOptions(options), workDir, generatorLog, consumer);
+            if (!executor.isSuccess()) {
+                LOG.warn("There was an error executing the generator command");
+            }
+
+        } catch (Exception e) {
+            LOG.warn("Error executing the generator", e);
+        }
+    }
+
 
     @Override
     public String getProjectType() {
         return AdaptiveProjectType.TYPE;
+    }
+
+    /**
+     * IMPORTER
+     */
+
+    @Override
+    public String getId() {
+        return "adaptive";
+    }
+
+    @Override
+    public boolean isInternal() {
+        return false;
+    }
+
+    @Override
+    public ImporterCategory getCategory() {
+        return ImporterCategory.SOURCE_CONTROL;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Generates an Adaptive Project based on the parameters passed";
+    }
+
+    @Override
+    public void importSources(FolderEntry baseFolder, String location, Map<String, String> parameters) throws ForbiddenException, ConflictException, UnauthorizedException, IOException, ServerException {
+        generateProject(baseFolder, Collections.emptyMap(), parameters, LineConsumerFactory.NULL);
+    }
+
+    @Override
+    public void importSources(FolderEntry baseFolder, String location, Map<String, String> parameters, LineConsumerFactory importOutputConsumerFactory) throws ForbiddenException, ConflictException, UnauthorizedException, IOException, ServerException {
+        generateProject(baseFolder, Collections.emptyMap(), parameters, importOutputConsumerFactory);
     }
 }
