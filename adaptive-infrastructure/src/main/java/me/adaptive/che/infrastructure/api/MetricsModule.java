e64bd1cc58a0bf14d0ba52ec4d68a2cdcb6a4d3e
Ferran Vilà Conesa <ikocel1@gmail.com>
2015-08-13 12:29:58 +0200

    Metrics module. Users count

--- adaptive-infrastructure/src/main/java/me/adaptive/che/infrastructure/api/MetricsModule.java
+++ adaptive-infrastructure/src/main/java/me/adaptive/che/infrastructure/api/MetricsModule.java
@@ -18,17 +18,73 @@
 
 package me.adaptive.che.infrastructure.api;
 
-import com.wordnik.swagger.annotations.Api;
+import com.google.inject.Inject;
+import com.wordnik.swagger.annotations.*;
+import me.adaptive.core.data.api.UserEntityService;
+import me.adaptive.core.data.api.UserRegistrationService;
+import me.adaptive.core.data.repo.UserRepository;
+import org.eclipse.che.api.core.ConflictException;
+import org.eclipse.che.api.core.NotFoundException;
+import org.eclipse.che.api.core.ServerException;
+import org.eclipse.che.api.core.UnauthorizedException;
 import org.eclipse.che.api.core.rest.Service;
+import org.eclipse.che.api.core.rest.annotations.GenerateLink;
+import org.eclipse.che.api.core.rest.annotations.Required;
+import org.eclipse.che.api.user.server.UserService;
+import org.eclipse.che.api.user.server.dao.User;
+import org.eclipse.che.api.user.shared.dto.UserDescriptor;
 import org.eclipse.che.inject.DynaModule;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
+import static org.eclipse.che.api.user.server.Constants.LINK_REL_CREATE_USER;
 
 /**
- * Created by panthro on 12/08/15.
+ * Module for exposing the metrics for the dashbar calls.
+ * Created by ferranvila on 12/08/15.
  */
 @DynaModule
 @Api(value = "/metrics", description = "Metrics Module")
 @Path("/metrics")
 public class MetricsModule extends Service {
+
+    UserEntityService userRegistrationService;
+
+    @Inject
+    public MetricsModule(UserEntityService userRegistrationService) {
+        this.userRegistrationService = userRegistrationService;
+    }
+
+    /**
+     * /metrics/user/builds/total/{platform} [android,ios,total]
+     * /metrics/user/builds/time/{platform} [android,ios]
+     *
+     * /metrics/builds/total/{platform} [android,ios,total]
+     * /metrics/builds/time/{platform} [android,ios]
+     *
+     * /metrics/users/total
+     *
+     * /metrics/server/{server}/{metric} [my.adaptive.me, infra1] [cpu,memory,file]
+     */
+
+    @ApiOperation(value = "Total number of users",
+            notes = "Returns the total number of users in the system",
+            response = String.class,
+            position = 1)
+    @ApiResponses({@ApiResponse(code = 200, message = "Successful operation"),
+            @ApiResponse(code = 500, message = "Internal Server Error")})
+    @GET
+    @Path("/users/total")
+    @GenerateLink(rel = "total users")
+    @Produces(TEXT_PLAIN)
+    public String totalUsers() throws ServerException {
+
+        return String.valueOf(userRegistrationService.count());
+    }
 }
