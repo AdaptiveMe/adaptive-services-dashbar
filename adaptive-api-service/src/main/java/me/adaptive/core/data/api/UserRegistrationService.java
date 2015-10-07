279f4ec45081b6989d1f535ed4c02f8381961864
Rafael Roman <panthro.rafael@gmail.com>
2015-10-07 16:11:22 +0200

    no need to flush since the jpa is in COMMIT flush mode

--- adaptive-api-service/src/main/java/me/adaptive/core/data/api/UserRegistrationService.java
+++ adaptive-api-service/src/main/java/me/adaptive/core/data/api/UserRegistrationService.java
@@ -114,7 +114,7 @@ public class UserRegistrationService {
             userEntity.getRoles().addAll(DEFAULT_USER_ROLES);
             userEntity.setUserId(NameGenerator.generate("user-", Constants.ID_LENGTH));
             userEntity.getPreferences().put(UserPreferences.Notification.EMAIL, email);
-            userEntity = userEntityService.saveAndFlush(userEntity);
+            userEntity = userEntityService.save(userEntity);
 
             /**
              * Profile
